fd6ef3d4964f4c46a806965114366111755003f7
mrsigni <155167113+mrsigni@users.noreply.github.com>
2026-01-11 06:53:19 +0800

    Integrate TiDB Cloud adapter in Prisma setup
    Refactor Prisma client setup to use TiDB Cloud adapter and streamline global variable assignment.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,20 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
+import { PrismaTiDBCloud } from '@tidbcloud/prisma-adapter'
+import { connect } from '@tidbcloud/serverless'
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+const connectionString = `${process.env.DATABASE_URL}`
+
+// Setup Adapter
+const connection = connect({ url: connectionString })
+const adapter = new PrismaTiDBCloud(connection)
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
 export const prisma =
-  globalForPrisma.prisma ??
+  globalForPrisma.prisma ||
   new PrismaClient({
-    log: ["error"], // Hanya log error
-  });
+    adapter, // <--- Gunakan adapter
+    log: ['error'],
+  })
 
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
